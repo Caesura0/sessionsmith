60bfe9eda02a8a1b968d2b1db92dc4a811930745
HIGH-COMMAND\jskar <jskarupa1@gmail.com>
2026-03-02 17:25:44 -0500

    fix(build): elevate globals.css to root src folder to bypass git case cache issues

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@
  */
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/globals.css";
+import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
