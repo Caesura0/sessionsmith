643bac4eec49616991db4a40bc27a356c20d2863
HIGH-COMMAND\jskar <jskarupa1@gmail.com>
2026-03-02 17:17:46 -0500

    fix(build): force git folder to lowercase styles for Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@
  */
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./Styles/globals.css";
+import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
